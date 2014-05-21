072d637ae6f7844301024ada1cbd7b8129ae0671
Sean Manhertz <sean.manhertz@gmail.com>
2014-05-21 16:21:38 -0600

    Added Scrolling background and random plane sprite; set starting location on game load

--- dist/js/game.js
+++ dist/js/game.js
@@ -101,23 +101,32 @@ module.exports = Menu;
 
   var cursors;
   var Sprite = this.sprite;
+  var Enemy;
+  var bgtile;
   function Play() {}
   Play.prototype = {
     create: function() {
-      // bgtile = this.game.add.tileSprite(0,0, this.game.stage.bounds.width, this.game.cache.getImage('background').height,'background');
+      
       
       this.game.physics.startSystem(Phaser.Physics.ARCADE);
-      Sprite = this.game.add.sprite(this.game.width/2, this.game.height/2, 'yeoman');
+      // bgtile = this.game.add.tileSprite(0,0, 800,600,'background');
+      bgtile = this.game.add.tileSprite(0,0, this.game.stage.bounds.width, this.game.stage.bounds.height,'background');
+
+      Sprite = this.game.add.sprite(this.game.width/4, this.game.height/2, 'yeoman');
+      Enemy = this.game.add.sprite(this.game.width/2, this.game.height/4, 'redPlane');
+
       Sprite.inputEnabled = true;
       
       this.game.physics.arcade.enable(Sprite);
+      this.game.physics.arcade.enable(Enemy);
+
       Sprite.body.collideWorldBounds = true;
       // x and y components
-      Sprite.body.bounce.setTo(1,1);
+      Sprite.body.bounce.setTo(0,0);
       Sprite.body.gravity.y=800;
       // Sprite.body.velocity.x = this.game.rnd.integerInRange(-500,500);
-      Sprite.body.velocity.x = this.game.rnd.integerInRange(200,200);
-
+      Sprite.body.velocity.x = 0;
+// this.game.rnd.integerInRange(0,5);
       // Sprite.body.velocity.y = this.game.rnd.integerInRange(-500,500);
       // Sprite.body.velocity.y = this.game.rnd.integerInRange(0,200);
 
@@ -127,7 +136,7 @@ module.exports = Menu;
       
     },
     update: function() {
-      // bgtile.tilePosition.x -= 1;
+      bgtile.tilePosition.x -= 5;
       if (cursors.up.isDown)
     {
         Sprite.body.velocity.y = -550;
